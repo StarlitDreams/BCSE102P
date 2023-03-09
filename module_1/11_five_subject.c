9326ca50a454df9f1a2fa71bfaf16527d1183c36
Hari <kharinarayanan.a2022@vitstudent.ac.in>
2023-03-09 21:42:57 +0530

    Update 11_five_subject

--- module_1/11_five_subject.c
+++ module_1/11_five_subject.c
@@ -18,41 +18,46 @@ int main()
     printf("Welcome to the grade calculator!!\n");
 
     //Taking the input
-    while(i<=5)
+    while(i<=4)
     {   
-        i++;
-        printf("Please enter the grade for subject %d : ",i);
+        printf("Please enter the grade for subject %d : ",i+1);
         scanf("%d",&grade[i]);
+        i++;
     }
 
     //Outputting the marks
 
     i=0;
-    while(i<5)
-    {
-        i++;
+    while(i<4)
+    {   
         if(grade[i]>80)
         {
-            printf("The grade for the subject %d is A+",i);
+            printf("The grade for the subject %d is A+ \n",i+1);
+            
         }
         else if (grade[i]>65&&grade[i]<=80)
         {
-            printf("The grade for subject %d is A",i);
+            printf("The grade for subject %d is A\n",i+1);
+            
         }
         else if (grade[i]>50&&grade[i]<=65)
         {
-            printf("The grade for the subject %d is B",i);
+            printf("The grade for the subject %d is B\n",i+1);
+            
         }
         else if (grade[i]>42&&grade[i]<=50)
         {
-            printf("The grade for the subject %d is C",i);
+            printf("The grade for the subject %d is C\n",i+1);
+            
         }
         else
         {
-            printf("The grade for the subject %d is FAIL");
-        }
+            printf("The grade for the subject %d is FAIL\n");
+            
+        };
 
-
+        i++;
+        continue;
         return 0;
     }
 }
