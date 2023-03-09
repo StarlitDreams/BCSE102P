f1955ecc1d19722eab54e7bd379ab0def0bd0bf7
Hari <kharinarayanan.a2022@vitstudent.ac.in>
2023-03-09 08:53:00 +0530

    completed_simple_interest

--- module_1/simple_interest.c
+++ module_1/simple_interest.c
@@ -7,11 +7,12 @@ int main()
 {
     int principle=0,rate=0,time=0,interest=0;
     printf("Please input the principle: ");
-    scanf("%d",principle);
+    scanf("%d", &principle);
     printf("\nPlease input the rate: ");
-    scanf("%d",rate);
+    scanf("%d",&rate);
     printf("\nPlease input the time: ");
-    scanf("%d",time);
+    scanf("%d",&time);
     interest=(principle+rate+time)/100;
     printf("The interest is %d",interest);
+
 }
