7db9d11aaab654a16462e66a0f670b1b73c15ead
Hari <kharinarayanan.a2022@vitstudent.ac.in>
2023-03-09 08:26:55 +0530

    Created_program_1

---
+++ module_1/program1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/*
+
+ Write a program in which to declare all data type like
+integer ,double,float,long int and print value in specified format
+
+*/
+int main()
+{
+    //Initializing the variables
+    int x=10;
+    double y=15.69;
+    float z=20.69;
+    long int a =30;
+
+    //Printing the variables
+    printf("The value of integer is: %d \n",x);
+    printf("The value of double is %f\n",y);
+    printf("The value of the float is: %f \n",z);
+    printf("The value of long int is: %d\n",a);
+
+    return 0;
+
+
+}
