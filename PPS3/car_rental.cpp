d212f3c995bb1ffe7811f928e7446cfab5471890
Hari <kharinarayanan.a2022@vitstudent.ac.in>
2023-05-03 13:33:42 +0530

    car_rental

---
+++ PPS3/car_rental.cpp
@@ -0,0 +1,18 @@
+/*
+Q12
+A car rental company wants to keep track of its fleet of cars.
+ Each car has a make, model, year, and rental price. 
+ The company wants to be able to calculate the total rental price of 
+ all its cars and also find the car with the highest rental price.
+  Design a program using OOPs and the "this" pointer to implement this scenario. 
+  Create a new car object and verify that all 
+  the attributes are set correctly using the "this" pointer
+
+For example:
+Input	Result
+Model
+Swift
+Rental Price 
+15000
+*/
+
